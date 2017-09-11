7a6759a429a3691b8658a8a5cd5b81322a6acd46
SubokLang <lamdor2010@gmail.com>
2017-09-11 20:38:16 +0000

    added get_all_crimes

--- crimemap.py
+++ crimemap.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import json
 
 import dbconfig
 if dbconfig.test:
@@ -11,14 +12,29 @@ else:
 app = Flask(__name__)
 DB = DBHelper
 
-@app.route("/")
-def home():
+@app.route('/test')
+def test():
     try:
         data = DB.get_all_inputs()
     except Exception as e:
-        print(e)
+        print (e)
+        # getting error - get_all_inputs() missing 1 required positional argument: 'self'
         data = None
     return render_template("home.html", data=data)
+
+@app.route("/")
+def home():
+    # crimes = DB.get_all_crimes()
+    # getting error - get_all_crimes() missing 1 required positional argument: 'self'
+    # hardcoded crimes
+    crimes = [{'latitude':37.3215697595007,
+                 'longitude':-121.97733879089355,
+                 'date':"2000-01-01",
+                 'category':"mugging",
+                 'description':'i was mugged'
+                 }]
+    crimes = json.dumps(crimes)
+    return render_template("home.html", crimes=crimes)
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -35,6 +51,16 @@ def clear():
         DB.clear_all()
     except Exception as e:
         print(e)
+    return home()
+
+@app.route("/submitcrime", methods=['POST'])
+def submitcrime():
+    category = request.form.get("category")
+    date = request.form.get("date")
+    latitude = float(request.form.get("latitude"))
+    longitude = float(request.form.get("longitude"))
+    description = request.form.get("description")
+    DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
 if __name__ == '__main__':
--- mockdbhelper.py
+++ mockdbhelper.py
@@ -10,3 +10,14 @@ class MockDBHelper:
 
     def clear_all(self):
         pass
+
+    def add_crime(self, category, date, latitude, longitude, description):
+        pass
+
+    def get_all_crimes(self):
+        return [{'latitude':37.3215697595007,
+                 'longitude':-121.97733879089355,
+                 'date':"2000-01-01",
+                 'category':"mugging",
+                 'description':'i was mugged'
+                 }]
